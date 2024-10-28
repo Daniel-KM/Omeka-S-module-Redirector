19fb03bedd07c3d76a7a84298f1d10a514e044c3
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-10-28 00:00:00 +0000

    Updated languages.

--- config/module.config.php
+++ config/module.config.php
@@ -16,6 +16,16 @@ return [
             Form\SiteSettingsFieldset::class => Form\SiteSettingsFieldset::class,
         ],
     ],
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => dirname(__DIR__) . '/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'redirector' => [
         'site_settings' => [
             'redirector_redirections' => [],

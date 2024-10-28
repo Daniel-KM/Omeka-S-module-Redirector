f6d88eb950714627c2943d86a109bbf38567b496
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-10-28 00:00:00 +0000

    Initial release.

---
+++ config/module.config.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Redirector;
+
+return [
+    'service_manager' => [
+        'invokables' => [
+            Mvc\MvcListeners::class => Mvc\MvcListeners::class,
+        ],
+    ],
+    'listeners' => [
+        Mvc\MvcListeners::class,
+    ],
+    'form_elements' => [
+        'invokables' => [
+            Form\SiteSettingsFieldset::class => Form\SiteSettingsFieldset::class,
+        ],
+    ],
+    'redirector' => [
+        'site_settings' => [
+            'redirector_redirections' => [],
+        ],
+    ],
+];
